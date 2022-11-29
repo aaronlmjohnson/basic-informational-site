475dd57c29c62cb8962e89ad40f0ffa953b6b377
Aaron <ajonaaronajon@yahoo.com>
2022-11-29 14:38:02 -0500

    added about, contact-me, and 404 pages

--- index.js
+++ index.js
@@ -3,16 +3,37 @@ const http = require('http');
 const path = require('path');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 8080;
 
 const server = http.createServer((req, res)=>{
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content)=>{
-        if(err) throw err;
-        res.writeHead(200, {'Content-Type': 'text/html'});
+    let pathName = getPathName(req);
+    let filePath = path.join(__dirname, 'public', pathName);
+    
+    let contentType = 'text/html';
+    fs.readFile(filePath, (err, content)=>{
+        if(err){
+            if(err.code === 'ENOENT'){
+                fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content)=>{
+                    res.writeHead(200, {'Content-Type': contentType});
+                    res.end(content, 'utf-8');
+                });
+            }
+        }
+
+        res.writeHead(200, {'Content-Type': contentType});
         res.end(content);
     });
-    
 });
+
+const getPathName = (req) =>{
+    let path = '';
+    if(req.url === '/') path = 'index.html';
+    else if(req.url === '/about') path = 'about.html';
+    else if(req.url === '/contact-me') path = 'contact-me.html';
+    else path = '404.html';
+    return path;
+}
+
 
 server.listen(port, hostname, ()=>{
     console.log(`Server running at http://${hostname}:${port}/`);
